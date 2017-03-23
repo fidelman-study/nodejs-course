4eeba5872339b3dc4f570ce80d6282663238cea5
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-03-23 11:34:50 +0100

    HW 2: Test for GET

--- 02/get-post-server/test/server.js
+++ 02/get-post-server/test/server.js
@@ -2,10 +2,12 @@ const assert = require('assert');
 
 const server = require('../server');
 const request = require('request');
+const fs = require('fs');
+const path = require('path');
 
 const sum = (a, b) => {
   return a + b;
-}
+};
 
 describe('sum tests', () => {
   describe('sum of 2 numbers', () => {
@@ -30,22 +32,38 @@ describe('server test suites', () => {
     app.close(done);
   });
 
-  it('should index.html', done => {
-    /*
-      1. run server +
-      2. make request "GET /"
-      3. wait response
-      4. compare content-type, body
-    */
+  describe('GET', () => {
+    it('should index.html', done => {
+      /*
+       1. run server +
+       2. make request "GET /"
+       3. wait response
+       4. compare content-type, body
+       */
 
-    request('http://localhost:3000', function (error, response, body) {
-      // console.log('error:', error); // Print the error if one occurred
-      // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      // console.log('body:', body); // Print the HTML for the Google homepage.
+      request('http://localhost:3000', function (error, response, body) {
+        // console.log('error:', error); // Print the error if one occurred
+        // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        // console.log('body:', body); // Print the HTML for the Google homepage.
 
-      assert.equal(response.headers['content-type'], 'text/html');
+        assert.equal(response.headers['content-type'], 'text/html');
 
-      done();
+        done();
+      });
+    });
+
+    it('should existing image', done => {
+      request('http://localhost:3000/big.png', function(error, response, body) {
+        assert.equal(response.headers['content-type'], 'image/png');
+        done();
+      });
+    });
+
+    it('should 404 if don\'t have the file', done => {
+      request('http://localhost:3000/asdadasd', function(error, response, body) {
+        assert.equal(response.statusCode, 404);
+        done();
+      });
     });
   });
 });
